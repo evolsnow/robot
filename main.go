d0fd0f184533d51bfe5905ca80862af958ddee1e
evolsnow <gsc1215225@gmail.com>
2016-01-14 22:47:27 +0800

    split

--- main.go
+++ main.go
@@ -80,6 +80,7 @@ func socketHandler(ws *websocket.Conn) {
 					working++
 					wait--
 					for i := range ret {
+						fmt.Sprintf("check wait:%d", wait)
 						if wait > 0 {
 							working--
 							log.Println("new comming")
