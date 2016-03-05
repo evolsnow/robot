2ff48b8d21defee67c78bcd5355a3525d5b6ffa0
evolsnow <evolsnow@users.noreply.github.com>
2016-03-05 21:06:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -72,7 +72,7 @@ func groupTalk(w http.ResponseWriter, r *http.Request) {
 			if visitor > 0 {
 				msgToTl := <-tlChan
 				replyFromTl := tlAI(msgToTl)
-				time.Sleep(time.Second)
+				time.Sleep(time.Second * 2)
 				go func() {
 					if replyFromTl != "" {
 						result <- "samaritan: " + replyFromTl
@@ -92,7 +92,7 @@ func groupTalk(w http.ResponseWriter, r *http.Request) {
 			if visitor > 0 {
 				msgToQin := <-qinChan
 				replyFromQin := qinAI(msgToQin)
-				time.Sleep(time.Second)
+				time.Sleep(time.Second * 2)
 				go func() {
 					if replyFromQin != "" {
 						result <- "菲菲: " + replyFromQin
@@ -166,7 +166,7 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 		ret = receive(string(in))
 		for i := range ret {
 			c.WriteMessage(mt, []byte(ret[i]))
-			time.Sleep(time.Second * 1.5)
+			time.Sleep(time.Second)
 		}
 		c.WriteMessage(mt, []byte(""))
 	}
